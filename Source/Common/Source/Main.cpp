c1e7e815ca23cf13fc5d2a49fdd655beb07d4fe9
Rico <teh_programerer@interactivelaboratories.com>
2013-12-14 15:51:31 +0000

    |BUILD CONFIGURATION| [Windows] * Added a Makefile which will work on Cygwin, 64BIT=True or 32BIT=True will   need to be passed into make for the builds to work correctly
    |SOURCE|
    [Common::Naub]
    * Check for Windows build added to use the correct entry point and header file
      for Windows-specific data types and functions

--- Source/Common/Source/Main.cpp
+++ Source/Common/Source/Main.cpp
@@ -1,7 +1,17 @@
 #include <GitVersion.hpp>
 #include <Game.hpp>
+#if defined LD_PLATFORM_WINDOWS
+#include <windows.h>
+#endif // LD_PLATFORM_WINDOWS
 
+#if defined LD_PLATFORM_WINDOWS
+WINAPI INT WinMain( HINSTANCE p_ThisInst, HINSTANCE p_PrevInstance,
+	LPSTR p_pCmdLine, INT p_CmdShow )
+#elif LD_PLATFORM_LINUX
 int main( int p_Argc, char **p_ppArgc )
+#else
+#error Unknown platform
+#endif // LD_PLATFORM_WINDOWS
 {
 	LD::Game TheGame;
 
